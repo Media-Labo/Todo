7ab0b21cb319f23ff5930419369b3f7524d1edef
Tanabe <6isstrong@gmail.com>
2013-03-15 08:59:30 +0900

    列挙型を新規作成
    状態を表す列挙を作成

---
+++ TodoManagement/src/jp/co/media_labo/android/todo/entities/ToDoStatus.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+package jp.co.media_labo.android.todo.entities;
+
+/**
+ * ToDoの状態を表します。
+ * @author tanabe
+ *
+ */
+public enum ToDoStatus {
+	CREATED,	// 作成直後で未着手
+	COMPLETED,	// 終了
+	DELETED,	// 削除
+}
---
+++ TodoManagement/src/jp/co/media_labo/android/todo/entities/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * データ保持クラスをまとめたパッケージです。
+ */
+/**
+ * @author tanabe
+ *
+ */
+package jp.co.media_labo.android.todo.entities;
